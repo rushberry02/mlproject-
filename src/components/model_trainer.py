03a8bd1af08129ca0fed631dba0692f91bb28b16
rushberry02 <rushberry02@gmail.com>
2026-08-19 01:10:25 +0530

    Hyperparamter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,11 +47,58 @@ class ModelTrainer:
             "Support Vector Regressor": SVR(),
             "XGBRegressor": XGBRegressor(),
             "CatBoostRegressor": CatBoostRegressor(verbose=False),
-            "AdaBoostRegressor": AdaBoostRegressor() 
+            "AdaBoostRegressor": AdaBoostRegressor(),
+            "Gradient Boosting": GradientBoostingRegressor()
+            }
+        
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64]
+                },
+                "k-Nearest Neighbors": {
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ["uniform", "distance"],
+                    'algorithm': ["auto", "ball_tree", "kd_tree"]
+                },
+                "Support Vector Regressor": {
+                    'kernel': ["linear", "rbf"],
+                    'C': [0.1, 1, 10],
+                    'gamma': ["scale", "auto"]
+                }
+                
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                              models=models)
+                                              models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
 
