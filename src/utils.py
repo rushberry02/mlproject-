4b888aae6944ad9d1393faebcf470ffd38497afe
rushberry02 <rushberry02@gmail.com>
2026-08-20 00:27:52 +0530

    Final Prediction

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+import pickle
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
@@ -44,5 +45,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)
     
-      
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+          
         
